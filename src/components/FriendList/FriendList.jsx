7438ba866f5f9f5dd9e753faee2b6823c70b4ec0
Borys <boris02374@gmail.com>
2025-04-29 23:28:09 +0300

    fixed Profile.css

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,9 +1,9 @@
 import PropTypes from 'prop-types';
 import FriendListItem from '../FriendListItem/FriendListItem.jsx';
-import css from './FriendList.module.css';
+import styles from './FriendList.module.css';
 
 const FriendList = ({ friends }) => (
-  <ul className={css.friendList}>
+  <ul className={styles.friendList}>
     {friends.map((friend) => (
       <FriendListItem
         key={friend.id}
