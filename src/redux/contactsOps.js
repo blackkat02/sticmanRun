235cb3fc6be1ab3e8484ea779a581620921f6f6f
Borys <boris02374@gmail.com>
2025-06-05 19:05:40 +0300

    added camp_ops

--- src/redux/contactsOps.js
+++ src/redux/contactsOps.js
@@ -3,11 +3,11 @@ import axios from 'axios';
 
 axios.defaults.baseURL = "https://683da2b3199a0039e9e643b7.mockapi.io";
 
-export const getContactsSliceThunk = createAsyncThunk(
+export const getCatalogCampersSliceThunk = createAsyncThunk(
   'contacts/fetchAll',
   async (_, thunkAPI) => {
     try {
-      const response = await axios.get("/contacts");
+      const response = await axios.get("/campers");
       return response.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
@@ -15,27 +15,12 @@ export const getContactsSliceThunk = createAsyncThunk(
   }
 );
 
-export const createContactsSliceThunk = createAsyncThunk(
-  'contacts/addContact',
-  async (contact, thunkAPI) => {
-    try {
-      const response = await axios.post("/contacts/", {
-        name: contact.name,
-        number: contact.number,
-      });
-      return response.data;
-    } catch (error) {
-      return thunkAPI.rejectWithValue(error.message);
-    }
-  }
-);
-
-export const removeContactsSliceThunk = createAsyncThunk(
-  'contacts/deleteContact',
+export const getDetailsCampersSliceThunk = createAsyncThunk(
+  'contacts/fetchAll',
   async (id, thunkAPI) => {
     try {
-      await axios.delete(`/contacts/${id}`);
-      return id;
+      const response = await axios.get(`/campers/${id}`);
+      return response.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
     }
