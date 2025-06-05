31b480529f3575e75d4210836aa09b0e036cb971
Borys <boris02374@gmail.com>
2025-06-05 19:47:39 +0300

    added campers_slice

---
+++ src/redux/catalogCampersSlice.js
@@ -0,0 +1,57 @@
+import { createSlice, createSelector } from '@reduxjs/toolkit';
+import { 
+  getCatalogCampersSliceThunk, 
+  getDetailsCampersSliceThunk
+} from "./contactsOps";
+
+const handlePending = (state) => {
+  state.isLoading = true;
+  state.isError = false;
+};
+
+const handleRejected = (state) => {
+  state.isLoading = false;
+  state.isError = true;
+};
+
+const catalogCampersSlice = createSlice({
+  name: 'contacts',
+  initialState: {
+    items: [],
+    isLoading: false,
+    isError: false,
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(getCatalogCampersSliceThunk.pending, handlePending)
+      .addCase(getCatalogCampersSliceThunk.fulfilled, (state, { payload }) => {
+        state.isLoading = false;
+        state.items = payload;
+      })
+      .addCase(getCatalogCampersSliceThunk.rejected, handleRejected)
+      
+      .addCase(getDetailsCampersSliceThunk.pending, handlePending)
+      .addCase(getDetailsCampersSliceThunk.fulfilled, (state, { payload }) => {
+        state.isLoading = false;
+        state.items.push(payload);
+      })
+      .addCase(getDetailsCampersSliceThunk.rejected, handleRejected)
+  },
+});
+
+export const selectCatalogCampersSlice = (state) => state.contacts.items;
+export const selectIsLoading = (state) => state.contacts.isLoading;
+export const selectError = (state) => state.contacts.isError;
+export const selectFilter = (state) => state.filters.name; //state.filters
+
+export const selectFilteredContacts = createSelector(
+  [selectContacts, selectFilter],
+  (contacts, filter) => {
+    const normalizedFilter = filter.toLowerCase();
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+  }
+);
+
+export const catalogCampersSliceReducer = contactsSlice.reducer;
