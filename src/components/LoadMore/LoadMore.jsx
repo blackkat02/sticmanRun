4d413a481a7960d0daeb790ccc42cbf6827efb4e
Borys <boris02374@gmail.com>
2025-06-13 23:01:46 +0300

    fixed LoadMore

--- src/components/LoadMore/LoadMore.jsx
+++ src/components/LoadMore/LoadMore.jsx
@@ -1,3 +1,49 @@
-const LoadMore = ({ onClick }) => {
-  return <button type="button">Load more</button>;
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  selectLoadMore,
+  selectIsLoading,
+  loadMoreItems, 
+} from '../../redux/catalogSlice'; 
+
+import Loader from '../Loader/Loader';
+
+const LoadMore = () => {
+  const dispatch = useDispatch();
+  const shouldShowLoadMore = useSelector(selectLoadMore); 
+  const isLoading = useSelector(selectIsLoading);
+
+  const handleClick = () => {
+    if (!isLoading && shouldShowLoadMore) {
+        dispatch(loadMoreItems());
+    }
+  };
+
+  if (!shouldShowLoadMore && !isLoading) {
+    return null;
+  }
+
+  if (isLoading && shouldShowLoadMore) {
+      return (
+          <div style={{ textAlign: 'center', padding: '20px' }}>
+              <Loader />
+          </div>
+      );
+  }
+
+  if (!shouldShowLoadMore && isLoading) {
+      return null; 
+  }
+
+  return (
+    <button
+      type="button"
+      onClick={handleClick}
+      disabled={isLoading}
+    >
+      Load more
+    </button>
+  );
 };
+
+export default LoadMore;
